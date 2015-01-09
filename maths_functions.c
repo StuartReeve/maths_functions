7d69bd8573f077bce282faeb28c954a2e2e9ad2a
Stuart  Alexander Reeve <P13201388@GH583-005.lec-admin.dmu.ac.uk>
2015-01-09 14:37:08 +0000

    added subtract function

--- maths_functions.c
+++ maths_functions.c
@@ -4,12 +4,19 @@ void add(int* num, int a)
 {
 	*num = *num + a;
 }
+
+void subtract(int* num, int a)
+{
+	*num -= a;
+}
 	
 
 int main()
 {
 	int num = 10;
 	add(&num,4);
+	printf("%i \n",num);
+	subtract(&num, 7);
 	printf("%i \n",num);
 	return 0;	
 }
